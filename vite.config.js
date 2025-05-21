df36513bc58b7e497a5fdbeffc846d916a81163f
= <=>
2025-05-21 23:21:41 +0700

    fix4

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Portfolio/', // <-- GANTI sesuai nama repo GitHub kamu
+  base: '/',  // Penting agar asset path sesuai root
   plugins: [react()],
-});
+})
