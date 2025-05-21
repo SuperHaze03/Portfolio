10ddd69f958adc8b985f0db76938a9d1d0cc7876
= <=>
2025-05-22 00:54:15 +0700

    fix: flash

--- src/app.jsx
+++ src/app.jsx
@@ -1,27 +1,23 @@
 import React from 'react';
+import { HashRouter } from 'react-router-dom';
 import Navbar from './components/navbar';
 import About from './pages/About';
 import Projects from "./components/Projects";
 import Contact from './pages/Contact';
 
 import './style.css';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-        <Route path="/" element={<Navigate to="/about" />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </Router>
+    <HashRouter>
+      <Navbar />
+      <main>
+        <section id="about"><About /></section>
+        <section id="portfolio"><Projects /></section>
+        <section id="contact"><Contact /></section>
+      </main>
+    </HashRouter>
   );
 }
-
 
 export default App;
