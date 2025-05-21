f9ce22fcdb87f830aa30721dc850037ff397db97
= <=>
2025-05-21 22:51:36 +0700

    Resolve merge conflict in app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,24 +1,11 @@
-<<<<<<< HEAD
 import React from 'react';
-=======
-import React from 'react'; 
->>>>>>> 12ead7e765aebaa0996dd3f08b4bb317d1060e61
 import Navbar from './components/navbar';
-import About from './pages/about';
+import About from './pages/About';
 import Projects from "./components/Projects";
 import Contact from './pages/Contact';
-<<<<<<< HEAD
 
 import './style.css';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-
-function App() {
-  return (
-    <Router> {/* Ganti ke HashRouter */}
-=======
-import './style.css';  
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-
 
 function App() {
   // Memeriksa jika aplikasi berjalan di localhost atau GitHub Pages
@@ -26,7 +13,6 @@ function App() {
 
   return (
     <Router basename={basename}>  {/* Menetapkan basename secara dinamis */}
->>>>>>> 12ead7e765aebaa0996dd3f08b4bb317d1060e61
       <div>
         <Navbar />
         <Routes>
